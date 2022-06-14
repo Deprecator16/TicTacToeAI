5c412161c74081d6280e5e021111451748c38998
Bassil Virk <bassillooney@outlook.com>
2022-06-13 22:57:38 -0400

    Removed timer, added easier player designation

--- playTicTacToe.py
+++ playTicTacToe.py
@@ -2,8 +2,6 @@ from TicTacToeGame import TicTacToe
 from TTTMinimaxAI import TTTMinimax
 from TTTAlphaBetaAI import TTTAlphaBeta
 
-import time
-
 def getHumanInput(player):
     while True:
         row = int(input('Enter row number for player ' + player + ': ')) - 1
@@ -14,26 +12,19 @@ def getHumanInput(player):
             return row, col
 
 def getAIInput(board, player):
-    minimaxStart = time.perf_counter()
-    m1, m2 = TTTMinimax(board, player)
-    minimaxElapsed = time.perf_counter() - minimaxStart
+    useMinimax = False
+    row, col = TTTMinimax(board, player) if useMinimax else TTTAlphaBeta(board, player)
+    return row, col
 
-    alphaBetaStart = time.perf_counter()
-    ab1, ab2 = TTTAlphaBeta(board, player)
-    alphaBetaElapsed = time.perf_counter() - alphaBetaStart
-
-    print('Minimax: ', m1, ' ', m2, ' in ', minimaxElapsed, ' seconds')
-    print('AlphaBeta: ', ab1, ' ', ab2, ' in ', alphaBetaElapsed, ' seconds')
-    print ('Minimax to AlphaBeta ratio: ', minimaxElapsed / alphaBetaElapsed)
-    return ab1, ab2
-
-def getPlayerXInput(board, isHuman):
+def getPlayerXInput(board):
+    isHuman = True
     if isHuman:
         return getHumanInput('X')
     else:
         return getAIInput(board, 'X')
 
-def getPlayerOInput(board, isHuman):
+def getPlayerOInput(board):
+    isHuman = False
     if isHuman:
         return getHumanInput('O')
     else:
@@ -42,7 +33,7 @@ def getPlayerOInput(board, isHuman):
 game = TicTacToe()
 print(game)
 while True:
-    row, col = getPlayerXInput(game.board, True) if game.player == 'X' else getPlayerOInput(game.board, False)
+    row, col = getPlayerXInput(game.board) if game.player == 'X' else getPlayerOInput(game.board)
     game.play(row, col)
     print(game)
     if game.isWinner('X'):
